158bfb974332b2303a427db3cf8accc146eb6710
Carlos Arturo Gómez Jiménez <carlos.gomez@wedoengineer.com>
2024-06-15 12:49:33 -0500

    actualziacion

--- src/main/java/com/floristeria/repository/ProductoRepository.java
+++ src/main/java/com/floristeria/repository/ProductoRepository.java
@@ -2,6 +2,13 @@ package com.floristeria.repository;
 
 import com.floristeria.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto,Long> {
+    void saveOrUpdate(Producto producto);
+    
+    ArrayList<Producto> getProducto();
+    
 }
